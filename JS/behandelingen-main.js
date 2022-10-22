c12ba75acff94e662960ea754c572b34d582f3cf
K1NGAARON <gerardaaron1997@gmail.com>
2022-10-22 11:09:47 +0200

    changes

--- JS/behandelingen-main.js
+++ JS/behandelingen-main.js
@@ -3,52 +3,52 @@ const behandelingenWrapper = document.querySelector('#services-row');
 const behandelingenArray = [
     {
         'name' : 'Spierontspanners',
-        'img' : '../IMG/behandelingen/behandeling1.jpg',
+        'img' : './IMG/behandelingen/behandeling1.jpg',
         'url' : '/behandelingen/spierontspanners.html',
     },
     {
         'name' : 'Fillers',
-        'img' : '../IMG/behandelingen/behandeling1.jpg',
+        'img' : './IMG/behandelingen/behandeling1.jpg',
         'url' : '/behandelingen/fillers.html',
     },
     {
         'name' : 'Collageen stimulatie',
-        'img' : '../IMG/behandelingen/behandeling1.jpg',
+        'img' : './IMG/behandelingen/behandeling1.jpg',
         'url' : '/behandelingen/collageen.html',
     },
     {
         'name' : 'Volume-herstellende fillers',
-        'img' : '../IMG/behandelingen/behandeling1.jpg',
+        'img' : './IMG/behandelingen/behandeling1.jpg',
         'url' : '/behandelingen/volume-herstellende-fillers.html',
     },
     {
         'name' : 'Liquid face lift',
-        'img' : '../IMG/behandelingen/behandeling1.jpg',
+        'img' : './IMG/behandelingen/behandeling1.jpg',
         'url' : '/behandelingen/liquid-face-lift.html',
     },
     {
         'name' : 'Skinboosters',
-        'img' : '../IMG/behandelingen/behandeling1.jpg',
+        'img' : './IMG/behandelingen/behandeling1.jpg',
         'url' : '/behandelingen/skinboosters.html',
     },
     {
         'name' : 'Anti-transpiratie',
-        'img' : '../IMG/behandelingen/behandeling1.jpg',
+        'img' : './IMG/behandelingen/behandeling1.jpg',
         'url' : '/behandelingen/anti-transpiratie.html',
     },
     {
         'name' : 'Hair fillers',
-        'img' : '../IMG/behandelingen/behandeling1.jpg',
+        'img' : './IMG/behandelingen/behandeling1.jpg',
         'url' : '/behandelingen/hair-fillers.html',
     },
     {
         'name' : 'Body -collageen stimulatie',
-        'img' : '../IMG/behandelingen/behandeling1.jpg',
+        'img' : './IMG/behandelingen/behandeling1.jpg',
         'url' : '/behandelingen/body-collageen-stimulatie.html',
     },
     {
         'name' : 'Laserbehandelingen',
-        'img' : '../IMG/behandelingen/behandeling1.jpg',
+        'img' : './IMG/behandelingen/behandeling1.jpg',
         'url' : '/behandelingen/laserbehandelingen.html',
     }
 ]
