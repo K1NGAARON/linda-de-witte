7b4a38d7fe3c6274260c81c28382bc3a17b695c2
K1NGAARON <gerardaaron1997@gmail.com>
2022-10-29 09:38:47 +0200

    behandelingen

--- JS/behandelingen-main.js
+++ JS/behandelingen-main.js
@@ -4,47 +4,47 @@ const behandelingenArray = [
     {
         'name' : 'Spierontspanners',
         'img' : 'IMG/behandelingen/spierontspanners.jpg',
-        'url' : 'linda-de-witte/behandelingen/spierontspanners.html',
+        'url' : 'behandelingen/spierontspanners.html',
     },
     {
         'name' : 'Fillers',
         'img' : 'IMG/behandelingen/fillers.jpg',
-        'url' : 'linda-de-witte/behandelingen/fillers.html',
+        'url' : 'behandelingen/fillers.html',
     },
     {
         'name' : 'Collageen stimulatie',
         'img' : 'IMG/behandelingen/collageen_stimulatie.jpg',
-        'url' : 'linda-de-witte/behandelingen/collageen.html',
+        'url' : 'behandelingen/collageen.html',
     },
     {
         'name' : 'Volume-herstellende fillers',
         'img' : 'IMG/behandelingen/volume-fillers.jpg',
-        'url' : 'linda-de-witte/behandelingen/volume-herstellende-fillers.html',
+        'url' : 'behandelingen/volume-herstellende-fillers.html',
     },
     {
         'name' : 'Liquid face lift',
         'img' : 'IMG/behandelingen/liquid-facelift.jpg',
-        'url' : 'linda-de-witte/behandelingen/liquid-face-lift.html',
+        'url' : 'behandelingen/liquid-face-lift.html',
     },
     {
         'name' : 'Skinboosters',
         'img' : 'IMG/behandelingen/skinboosters.jpg',
-        'url' : 'linda-de-witte/behandelingen/skinboosters.html',
+        'url' : 'behandelingen/skinboosters.html',
     },
     {
         'name' : 'Anti-transpiratie',
         'img' : 'IMG/behandelingen/transpiratie.jpg',
-        'url' : 'linda-de-witte/behandelingen/anti-transpiratie.html',
+        'url' : 'behandelingen/anti-transpiratie.html',
     },
     {
         'name' : 'Hair fillers',
         'img' : 'IMG/behandelingen/hair-fillers.jpg',
-        'url' : 'linda-de-witte/behandelingen/hair-fillers.html',
+        'url' : 'behandelingen/hair-fillers.html',
     },
     {
         'name' : 'Body -collageen stimulatie',
         'img' : 'IMG/behandelingen/body-collageen.jpg',
-        'url' : 'linda-de-witte/behandelingen/body-collageen-stimulatie.html',
+        'url' : 'behandelingen/body-collageen-stimulatie.html',
     },
     {
         'name' : 'Laserbehandelingen',
